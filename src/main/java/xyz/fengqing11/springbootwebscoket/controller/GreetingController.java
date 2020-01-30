da62b41b3b90bf717ec58895a07e35943fa60ac3
fengqing11 <tanzi8845@gmail.com>
2020-01-30 13:48:12 +0800

    点对点

--- src/main/java/xyz/fengqing11/springbootwebscoket/controller/GreetingController.java
+++ src/main/java/xyz/fengqing11/springbootwebscoket/controller/GreetingController.java
@@ -1,15 +1,29 @@
 package xyz.fengqing11.springbootwebscoket.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import xyz.fengqing11.springbootwebscoket.pojo.Chat;
 import xyz.fengqing11.springbootwebscoket.pojo.Message;
+
+import java.security.Principal;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Message greeting(Message message) throws Exception {
         return message;
+    }
+    @MessageMapping("/chat")
+    public void chat(Principal principal, Chat chat) {
+        String from = principal.getName();
+        chat.setFrom(from);
+        messagingTemplate.convertAndSendToUser(chat.getTo(),
+                "/queue/chat", chat);
     }
 }
